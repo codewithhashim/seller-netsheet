129afd0a08b32a22899b5caf4e02eba3ddff3941
prohashim <prohashim4e@gmail.com>
2024-01-17 16:47:14 +0500

    some changed made

--- app.js
+++ app.js
@@ -7,19 +7,22 @@ document.addEventListener("DOMContentLoaded", function () {
   // Get output elements
   const sellingPriceOutput = document.getElementById("price");
   const mortgagePayoffOutput = document.getElementById("morgage-value");
-  const commissionOutput = document.getElementById("commission-value");
+  const commissionOutput = document.getElementsByClassName("commission-value");
   const titleInsuranceOutput = document.getElementById("title-ins");
   const titleSearchOutput = document.getElementById("title-search");
-  const stateDocStampsOutput = document.querySelector(".state-ds");
-  const lienSearchOutput = document.querySelector(".lien-search");
+  const stateDocStampsOutput = document.querySelector("#state-ds");
+  const lienSearchOutput = document.querySelector("#lien-search");
   const closingFeeOutput = document.getElementById("closing-fee");
   const brokerCommissionOutput = document.getElementById("commission-result");
   const estimatedProceedsOutput = document.getElementById("estimated-proceeds");
 
   // Set default values
-  sellingPriceInput.value = 234099887766;
-  mortgagePayoffInput.value = 300900;
+  sellingPriceInput.value = 100000;
+  mortgagePayoffInput.value = 0;
   commissionInput.value = 3.5;
+
+  // Call updateOutputs initially to set default values
+  updateOutputs();
 
   // Update outputs on input change
   sellingPriceInput.addEventListener("input", updateOutputs);
@@ -56,8 +59,10 @@ document.addEventListener("DOMContentLoaded", function () {
     mortgagePayoffOutput.textContent = mortgagePayoff
       .toFixed(2)
       .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    if (commissionOutput) {
-      commissionOutput.textContent = `${commissionRate.toFixed(2)}%`;
+    if (commissionOutput.length > 0) {
+      for (let i = 0; i < commissionOutput.length; i++) {
+        commissionOutput[i].textContent = `${commissionRate.toFixed(2)}`;
+      }
     }
     titleInsuranceOutput.textContent = titleInsurance
       .toFixed(2)
@@ -108,3 +113,59 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
+// range slider 1
+const slider_input = document.getElementById("morgage-pay"),
+  slider_thumb = document.getElementById("slider_thumb"),
+  slider_line = document.getElementById("slider_line");
+
+function showSliderValue() {
+  slider_thumb.innerHTML = slider_input.value;
+
+  const bulletPosition = slider_input.value / slider_input.max,
+    space = slider_input.offsetWidth - slider_thumb.offsetWidth;
+
+  const thumbPosition = bulletPosition * space > 0 ? bulletPosition * space : 0;
+
+  slider_thumb.style.left = thumbPosition + "px";
+  slider_line.style.width = bulletPosition * 100 + "%";
+}
+
+showSliderValue();
+window.addEventListener("resize", showSliderValue);
+slider_input.addEventListener("input", showSliderValue, false);
+
+// range slider 2
+
+const slider_inputc = document.getElementById("commission"),
+  slider_thumbc = document.getElementById("slider_thumbc"),
+  slider_linec = document.getElementById("slider_linec");
+
+function showSliderValuec() {
+  slider_thumbc.innerHTML = slider_inputc.value;
+
+  const bulletPositionc = slider_inputc.value / slider_inputc.max,
+    spacec = slider_inputc.offsetWidth - slider_thumbc.offsetWidth;
+
+  const thumbPositionc =
+    bulletPositionc * spacec > 0 ? bulletPositionc * spacec : 0;
+
+  slider_thumbc.style.left = thumbPositionc + "px";
+  slider_linec.style.width = bulletPositionc * 100 + "%";
+}
+
+showSliderValuec();
+window.addEventListener("resize", showSliderValuec);
+slider_inputc.addEventListener("input", showSliderValuec, false);
+
+// toggle dropdown
+function toggleTopSection() {
+  const topSection = document.getElementById("toggle-results");
+  const arrowImage = document.getElementById("calc-toggle");
+
+  topSection.classList.toggle("collapsed");
+
+  // Toggle the rotation of the arrow image
+  const isCollapsed = topSection.classList.contains("collapsed");
+  arrowImage.style.transform = isCollapsed ? "rotate(0deg)" : "rotate(180deg)";
+}
